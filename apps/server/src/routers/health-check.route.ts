2db4553c38c732f05933866c93d34016dd295244
milon27 <mdjahidulislammilon@gmail.com>
2024-06-22 10:08:00 +0600

    remove buged one

--- apps/server/src/routers/health-check.route.ts
+++
@@ -1,5 +0,0 @@
-import { publicProcedure, router } from "../trpc";
-
-export const healthRouter = router({
-  check: publicProcedure.query(() => "hello tRPC v10!"),
-});
--- apps/server/src/routers/index.ts
+++
@@ -1,6 +0,0 @@
-import { router } from "../trpc";
-import { healthRouter } from "./health-check.route";
-
-export const appRouter = router({
-  health: healthRouter,
-});
